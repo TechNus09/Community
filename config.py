d03f7970e4fa27d7a195ce88f5f4ca0b0a540287
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2022-04-10 06:47:02 +0100

    my dumbass forgot to remove bot token

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+import os
 """the channel id where the applications will be sent"""
 CHANNEL_ID = 839662151010353175
+
+"""the bot token saved in env vars as 'TOKEN' """
+TOKEN = os.getenv("TOKEN")
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import datetime
 import time
 import math
 import config
+from config import TOKEN
 
 
 
 presence = it.PresenceActivity(name="Over CoA Community", type=it.PresenceActivityType.WATCHING)
-bot = Client(token="ODgxMTc4MzEzMTYxMzEwMjI4.YSpDQQ.4AxnErFFasM_mmQLlLBNihX0ovs",presence=it.ClientPresence(activities=[presence]),disable_sync=False)
+bot = Client(token=TOKEN,presence=it.ClientPresence(activities=[presence]),disable_sync=False)
 
 
 @bot.event
